109df483d9b968e308082b962e3f00715f7688eb
Marius Kasparek <marius.kasparek@gmail.com>
2025-03-25 11:10:41 +0100

    added dynamic images to sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -7,10 +7,10 @@ const Sidebar = () => {
   const location = useLocation();
 
   const getImageSrc = (path) => {
-    if (location.pathname === path) {
-      return `assets/img/${path.replace('/', '')}1.png`; 
-    }
-    return `assets/img/${path.replace('/', '')}.png`; 
+    const formattedPath = path.replace('/', ''); // Remove leading slash if any
+    return location.pathname === `/${formattedPath}`
+      ? `assets/img/${formattedPath}1.png`
+      : `assets/img/${formattedPath}.png`;
   };
 
     return (
@@ -18,20 +18,20 @@ const Sidebar = () => {
         <img className="sidebar-logo" src="/assets/img/logo_light.png" alt="Logo" />
         
         <div className="sidebar-menu">
-          <NavLink to="/summary" className="sidebar-menu-div summary-div" activeClassName="active-link">
-            <img className="sidebar-icon" src="/assets/img/vector1.png" alt="Summary Icon" />
+          <NavLink to="/summary" className="sidebar-menu-div summary-div" >
+            <img className="sidebar-icon" src={getImageSrc('summary')} alt="Summary Icon" />
             <span>Summary</span>
           </NavLink>
-          <NavLink to="/add-task" className="sidebar-menu-div add-task-div" activeClassName="active-link">
-            <img className="sidebar-icon" src="/assets/img/edit_square.png" alt="Add Task Icon" />
+          <NavLink to="/add-task" className="sidebar-menu-div add-task-div" >
+            <img className="sidebar-icon" src={getImageSrc('add_task')} alt="Add Task Icon" />
             <span>Add Task</span>
           </NavLink>
-          <NavLink to="/board" className="sidebar-menu-div board-div" activeClassName="active-link">
-            <img className="sidebar-icon" src="/assets/img/Vector.png" alt="Board Icon" />
+          <NavLink to="/board" className="sidebar-menu-div board-div" >
+            <img className="sidebar-icon" src={getImageSrc('board')} alt="Board Icon" />
             <span>Board</span>
           </NavLink>
-          <NavLink to="/contacts" className="sidebar-menu-div contacts-div" activeClassName="active-link">
-            <img className="sidebar-icon" src="/assets/img/perm_contact_calendar.png" alt="Contacts Icon" />
+          <NavLink to="/contacts" className="sidebar-menu-div contacts-div" >
+            <img className="sidebar-icon" src={getImageSrc('contacts')} alt="Contacts Icon" />
             <span>Contacts</span>
           </NavLink>
         </div>
