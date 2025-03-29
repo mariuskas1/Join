22d13adb48c9edc5e59beb8083ed1203be52c036
Marius Kasparek <marius.kasparek@gmail.com>
2025-03-29 12:37:38 +0100

    ActiveContactModal component

---
+++ src/components/ActiveContactModal/ActiveContactModal.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./../../index.css";
+import "./ActiveContact.css";
+import "../../pages/Contacts/Contacts.css";
+
+
+const ActiveContactModal = ({ contact, onEdit, onDelete }) => {
+
+  return (
+    <div>
+      <div className="single-contact-display-header">
+        <div
+          className="scd-initials"
+          style={{ backgroundColor: contact.color }}
+        >
+          {contact.initials}
+        </div>
+        <div className="scd-name-div">
+          <span className="scd-name">{contact.name}</span>
+          <div className="scd-options-div">
+            <button className="scd-option-btn" onClick={onEdit}>
+              <img src="assets/img/edit.png" alt="Edit" /> Edit
+            </button>
+            <button className="scd-option-btn" onClick={onDelete}>
+              <img src="assets/img/delete.png" alt="Delete" /> Delete
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="single-contact-display-body">
+        <span className="contact-info">Contact Information</span>
+        <span className="scd-info-title">Email</span>
+        <span className="contact-mail">{contact.mail}</span>
+        <span className="scd-info-title">Phone</span>
+        <span className="contact-phone">{contact.phone}</span>
+      </div>
+    </div>
+  );
+};
+
+export default ActiveContactModal;
