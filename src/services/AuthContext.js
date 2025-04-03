e0c69be6d274431c7905a1572c96bd187152f5c6
Marius Kasparek <marius.kasparek@gmail.com>
2025-04-03 12:06:08 +0200

    added AuthContext.js and ProtectedRoute.js

---
+++ src/services/AuthContext.js
@@ -0,0 +1,40 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const checkUserAuthentication = async () => {
+            const storedUser = localStorage.getItem("currentUser");
+
+            if (!storedUser) {
+                navigate("/");
+                return;
+            }
+
+            const isUserTokenValid = await tryFetchWithAuthentication();
+            if (!isUserTokenValid) {
+                localStorage.removeItem("currentUser");
+                setUser(null);
+                navigate("/");
+            } else {
+                setUser(storedUser);
+            }
+        };
+
+        checkUserAuthentication();
+    }, [navigate]);
+
+    return (
+        <AuthContext.Provider value={{ user }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => useContext(AuthContext);
---
+++ src/services/ProtectedRoute.js
@@ -0,0 +1,11 @@
+import { useAuth } from "./AuthContext";
+import { Navigate } from "react-router-dom";
+
+
+const ProtectedRoute = ({ children }) => {
+    const { user } = useAuth();
+    return user ? children : <Navigate to="/" />;
+};
+
+
+export default ProtectedRoute;
